af97d55f1fcdb6185b845bbb5d54ea4b33dadf61
possiblyMikeB <miblackmon@davidson.edu>
2022-07-22 12:01:44 -0400

    enable cpu % tracking

--- config/jupyter_lab_config.py
+++ config/jupyter_lab_config.py
@@ -1,5 +1,23 @@
 import os
 
-## turn on collaborative editing
+## turn on/off collaborative editing
 c.LabApp.collaborative = False
+
+
+import os
+
+## setup resource monitor
+#  (if SLURM environment variables defined)
+
+# memory
+if 'SLURM_MEM_PER_NODE' in os.environ:
+    c.ResourceUseDisplay.mem_limit = int(os.environ['SLURM_MEM_PER_NODE'])
+    c.ResourceUseDisplay.mem_limit *= 1024*1024 # rescale to bytes
+    pass
+
+# cpu
+if 'SLURM_CPUS_PER_TASK' in os.environ:
+    c.ResourceUseDisplay.track_cpu_percent = True
+    # c.ResourceUseDisplay.cpu_limit = float(os.environ['SLURM_CPUS_PER_TASK'])
+    pass
 
